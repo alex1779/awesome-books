41262333b3de3a88e860b8a1b8a48faea5fae557
alex1779 <alex1779@gmail.com>
2023-01-09 18:52:22 -0300

    Fix linters issues.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 let listBooks = [];
-//Local Storage
+// Local Storage
 const saveToLocalStorage = () => {
   localStorage.setItem('Library', JSON.stringify(listBooks));
 };
@@ -13,34 +13,36 @@ const getDataFromLocalStorage = () => {
 
 function displayBooks() {
   const section = document.querySelector('#book-list');
-  let books = ``;
+  let books = '';
   getDataFromLocalStorage();
 
   listBooks.forEach((book, index) => {
     books += `<article class="book">
     <p>${book.title} <br> ${book.author}</p>
-    <button type="button" id="${index}" class="remove-btn">Remove</button>
+    <button type="button" id="${index}" class="remove-btn" onclick="removeBook(${index})">Remove</button>
     <hr>
   </article>`;
   });
 
   if (listBooks.length === 0) {
-    books = `<p>Library is empty...</p>`;
+    books = '<p>Library is empty...</p>';
   }
   section.innerHTML = books;
-  // Traget all the removeBtn
-  const removeBtns = document.querySelectorAll('.remove-btn');
-  removeBtns.forEach((btn) => {
-    btn.addEventListener('click', removeBook);
-  });
+}
+
+function removeBook(id) {
+  const bookId = parseInt(id, 2);
+  listBooks = listBooks.filter((book, index) => index !== bookId);
+  saveToLocalStorage();
+  displayBooks();
 }
 
 function addBook() {
   const title = document.querySelector('#title');
   const author = document.querySelector('#author');
-  let bookTitle = title.value;
-  let bookAuthor = author.value;
-  if (bookTitle.trim().length != 0 && bookAuthor.trim().length != 0) {
+  const bookTitle = title.value;
+  const bookAuthor = author.value;
+  if (bookTitle.trim().length !== 0 && bookAuthor.trim().length !== 0) {
     const objBook = { title: bookTitle, author: bookAuthor };
     listBooks.push(objBook);
     saveToLocalStorage();
@@ -56,11 +58,5 @@ form.addEventListener('submit', (event) => {
   addBook();
 });
 
-function removeBook(event) {
-  const bookId = parseInt(event.target.id);
-  listBooks = listBooks.filter((book, index) => index !== bookId);
-  saveToLocalStorage();
-  displayBooks();
-}
-
 displayBooks();
+removeBook();
